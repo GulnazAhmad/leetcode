d106524cb6a95812b1b4f5bc3a89bbeadfd410e3
Gulnaz Ahmad <130208290+killua0393@users.noreply.github.com>
2025-04-03 19:44:24 +0530

    Time: 3 ms (25.00%) | Memory: 18 MB (25.93%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -9,10 +9,10 @@ class Solution:
         x=min(nw1,nw2)
         while(r<=x):
             if str1[l:r] == str2[l:r]:  # Check if substrings match
-
-                if(nw1%len(str1[l:r])==0 and nw2%len(str2[l:r])==0):
-                    if (str1[l:r] * (nw1 // len(str1[l:r])) == str1 and str2[l:r] * (nw2 // len(str2[l:r])) == str2):
-                        maxi=max(maxi,str1[l:r])
+                f=str1[l:r]
+                if(nw1%len(f)==0 and nw2%len(f)==0):
+                    if (f * (nw1 // len(f)) == str1 and f * (nw2 // len(f)) == str2):
+                        maxi=max(maxi,f)
             
             r+=1
         return maxi
