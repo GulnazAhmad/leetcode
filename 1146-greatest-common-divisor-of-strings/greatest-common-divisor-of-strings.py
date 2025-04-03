48745a6fabd7a3171747ecffaef1224772bf8d27
Gulnaz Ahmad <130208290+killua0393@users.noreply.github.com>
2025-04-03 19:41:12 +0530

    Time: 6 ms (11.67%) | Memory: 18.1 MB (7.89%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,20 +1,20 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        l1=0
-        r1=1
-        l2=0
-        r2=1
+        l=0
+        r=1
+        
         nw1=len(str1)
         nw2=len(str2)
         maxi=""
-        while(r1<=nw1 or r2<=nw2):
-            if(str1[l1:r1]==str2[l2:r2]):
-                if(nw1%len(str1[l1:r1])==0 and nw2%len(str2[l2:r2])==0):
-                    if (str1[l1:r1]*(nw1//len(str1[l1:r1]))==str1 and str2[l2:r2]*(nw2//len(str1[l2:r2]))==str2):
-                        maxi=max(maxi,str1[l1:r1])
+        x=min(nw1,nw2)
+        while(r<=x):
+            if str1[l:r] == str2[l:r]:  # Check if substrings match
+
+                if(nw1%len(str1[l:r])==0 and nw2%len(str2[l:r])==0):
+                    if (str1[l:r] * (nw1 // len(str1[l:r])) == str1 and str2[l:r] * (nw2 // len(str2[l:r])) == str2):
+                        maxi=max(maxi,str1[l:r])
             
-            r1+=1
-            r2+=1
+            r+=1
         return maxi
 
         
