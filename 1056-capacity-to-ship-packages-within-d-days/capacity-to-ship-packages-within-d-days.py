a1b2250f5ac3ae60592d46e9bcf5c963872fea80
Gulnaz Ahmad <130208290+killua0393@users.noreply.github.com>
2025-04-18 20:14:24 +0530

    Time: 189 ms (59.56%) | Memory: 22.3 MB (19.04%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,31 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        def valid(weights,capacity,days):
+            #maxi=0
+            sum=0
+            tday=1
+            for i in weights:
+                if(sum+i<=capacity):
+                    sum=sum+i
+                else:
+                    #maxi=max(maxi,sum)
+                    sum=i
+                    tday+=1
+            print("day",tday+1)
+            print(sum)
+            if(tday<=days):
+                return True
+            else:
+                return False
+            
+
+        l=max(weights)
+        r=sum(weights)
+        while(l<=r):
+            mid=(l+r)//2
+            print("capacity",mid)
+            if valid(weights,mid,days):
+                r=mid-1
+            else:
+                l=mid+1
+        return l   
