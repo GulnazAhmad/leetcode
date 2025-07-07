4907987adc011010460c5d376510db45ca7f2263
Gulnaz Ahmad <130208290+GulnazAhmad@users.noreply.github.com>
2025-07-07 16:22:20 +0530

    Time: 1 ms (90.76%) | Memory: 44.9 MB (74.49%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -1,58 +1,26 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
         if (root == null) return res;
 
-        Queue<TreeNode> q=new LinkedList<>();
+        Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        q.add(null);//q=1 null
-        List<Integer> list = new ArrayList<>();
 
-        while(!q.isEmpty()){
-            TreeNode currNode=q.remove(); //1 q=null
-            if(currNode==null){
-                res.add(new ArrayList<>(list)); //add current level
-                list.clear();//clear for next level
-                if(q.isEmpty()){
-                    break;
-                }
-                else{
-                    q.add(null); //q=2,3,null
-                }
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
 
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = q.poll();
+                level.add(curr.val);
 
-
-            }
-            else{
-                list.add(currNode.val);//"1 "
-                if(currNode.left!=null){
-                    q.add(currNode.left); //q=null ,2
-                }
-                if(currNode.right!=null){
-                    q.add(currNode.right); //q=null,2,3
-                }
-
-
-                
-
+                if (curr.left != null) q.add(curr.left);
+                if (curr.right != null) q.add(curr.right);
             }
 
+            res.add(level);
         }
-      return res;  
+
+        return res;
     }
 }
