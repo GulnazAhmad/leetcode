098bf7dd669c8d9146efdf57274f68ee661d2d2b
Gulnaz Ahmad <130208290+GulnazAhmad@users.noreply.github.com>
2025-07-07 16:16:26 +0530

    Time: 2 ms (6.40%) | Memory: 42.2 MB (42.90%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return new ArrayList<>();
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);
+        q.add(null);//q=1 null
+        List<Integer> list = new ArrayList<>();
+        while(!q.isEmpty())
+        {
+            TreeNode currNode=q.remove(); //1 q=null
+            if(currNode==null){
+                res.add(new ArrayList<>(list)); //add current level
+                list.clear();//clear for next level
+                if(q.isEmpty()){
+                    break;
+                }
+                else{
+                    q.add(null); //q=2,3,null
+                }
+            }
+            else{
+                list.add(currNode.val);//"1 "
+                if(currNode.left!=null){
+                    q.add(currNode.left); //q=null ,2
+                }
+                if(currNode.right!=null){
+                    q.add(currNode.right); //q=null,2,3
+                }
+            }
+
+        }
+    List<Integer> right = new ArrayList<>();
+    for (int i=0;i<res.size();i++){
+        List<Integer> innerList = res.get(i);
+        right.add(innerList.get(innerList.size()-1));
+      
+    } 
+    
+      return right;  
+        
+    }
+}
