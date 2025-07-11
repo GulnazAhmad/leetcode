a679d4d948da5805dc5ba296c45fae2ce870057d
Gulnaz Ahmad <130208290+GulnazAhmad@users.noreply.github.com>
2025-07-11 16:47:22 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (70.78%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
@@ -5,18 +5,11 @@ class Solution {
         for(int i=1;i<=n;i++){
             if(i%m!=0){
                 num1=num1+i;
-                System.out.println(num1);
-
             }
             else if(i%m==0){
-                                System.out.println(num2);
-
                 num2=num2+i;
             }
         }
-        System.out.println(num1);
-                System.out.println(num2);
-
         return num1-num2;
     }
 }
