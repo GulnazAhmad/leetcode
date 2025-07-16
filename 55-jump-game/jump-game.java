aa76c179dc8eebe88ec1da54b59f823f64163786
Gulnaz Ahmad <130208290+GulnazAhmad@users.noreply.github.com>
2025-07-16 13:28:25 +0530

    Time: 3 ms (50.79%) | Memory: 45.9 MB (31.60%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        Set<Integer> set=new HashSet<>();
+        int l=nums.length;
+        int maxindex=0;
+        for(int i=0;i<l;i++){
+            int x=nums[i]+i;
+            if(i>maxindex){
+                return false;
+            }
+                        maxindex=Math.max(maxindex,x);
+
+
+        }
+        return true;
+        
+    }
+}
