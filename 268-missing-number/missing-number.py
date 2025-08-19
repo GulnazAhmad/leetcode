e47c0cbd80f4264d910a517b9b48459a1468cc75
Gulnaz Ahmad <130208290+GulnazAhmad@users.noreply.github.com>
2025-08-19 16:35:18 +0530

    Time: 1 ms (67.12%) | Memory: 19.2 MB (5.85%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,5 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        #using set cause searching in set is O(1) but in list directly is O(n).
         num_set=set(nums);
         for i in range(len(nums)):
             if(i not in num_set):
